139a7ed36b2a5846447bd852df3cd2b867dafcf2
Andrew Kim <taeyeonkim90@gmail.com>
2018-02-13 14:54:54 -0800

    finished creating REST endpoints for Alarm model

--- backend/home_automation_project/lumos/models.py
+++ backend/home_automation_project/lumos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from rest_framework import serializers
 
+
 class AlarmSchedule(models.Model):
     """
     Cron-based representation of a scheduled task
@@ -16,28 +17,44 @@ class AlarmSchedule(models.Model):
 
 
 class AlarmScheduleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     minute = serializers.CharField(max_length=100)
     hour = serializers.CharField(max_length=100)
     day_of_week = serializers.CharField(max_length=100)
     command = serializers.CharField(max_length=100)
 
     def validate_minute(self, val):
-        minutes = [str(x) for x in range(0,61)]
+        minutes = [str(x) for x in range(0, 61)]
         minutes.append("*")
         if val not in minutes:
-            raise serializers.ValidationError("Invalid input for scheduling an alarm task")
+            raise serializers.ValidationError("Invalid input for scheduling an \
+                                              alarm task")
         return val
 
     def validate_hour(self, val):
-        hours = [str(x) for x in range(0,25)]
+        hours = [str(x) for x in range(0, 25)]
         hours.append("*")
         if val not in hours:
-            raise serializers.ValidationError("Invalid input for scheduling an alarm task")
+            raise serializers.ValidationError("Invalid input for scheduling an \
+                                              alarm task")
         return val
     
     def validate_day_of_week(self, val):
-        days = [str(x) for x in range(0,7)]
+        days = [str(x) for x in range(0, 7)]
         days.append("*")
         if val not in days:
-            raise serializers.ValidationError("Invalid input for scheduling an alarm task")
+            raise serializers.ValidationError("Invalid input for scheduling an \
+                                              alarm task")
         return val
+    
+    def create(self, validated_data):
+        return AlarmSchedule.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.minute = validated_data.get("minute", instance.minute)
+        instance.hour = validated_data.get("hour", instance.hour)
+        instance.day_of_week = validated_data.get("day_of_week", 
+                                                  instance.day_of_week)
+        instance.command = validated_data.get("command", instance.command)
+        instance.save()
+        return instance
--- backend/home_automation_project/lumos/views.py
+++ backend/home_automation_project/lumos/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import AlarmSchedule, AlarmScheduleSerializer
+
+
+class AlarmList(APIView):
+    """
+    List all alarms, or create a new alarm.
+    """
+    def get(self, request, format=None):
+        alarms = AlarmSchedule.objects.all()
+        serializer = AlarmScheduleSerializer(alarms, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = AlarmScheduleSerializer(data=request.data)
+        if serializer.is_valid():
+            alarm = serializer.save()
+            serializer = AlarmScheduleSerializer(alarm)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AlarmDetail(APIView):
+    """
+    Retrieve, update or delete a alarm instance.
+    """
+    def get_object(self, pk):
+        try:
+            return AlarmSchedule.objects.get(pk=pk)
+        except AlarmSchedule.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        alarm = self.get_object(pk)
+        serializer = AlarmScheduleSerializer(alarm)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        alarm = self.get_object(pk)
+        serializer = AlarmScheduleSerializer(alarm, data=request.data)
+        if serializer.is_valid():
+            alarm = serializer.save()
+            serializer = AlarmScheduleSerializer(alarm)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        alarm = self.get_object(pk)
+        alarm.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
