02fd93e3665e60dc02f291b860c8657593d5b01a
Andrew Kim <taeyeonkim90@gmail.com>
2018-02-13 22:55:31 -0800

    refactored out json creating scripts for reusability in testing

--- backend/home_automation_project/lumos/tests.py
+++ backend/home_automation_project/lumos/tests.py
@@ -7,25 +7,38 @@ from rest_framework.renderers import JSONRenderer
 from .models import AlarmSchedule, AlarmScheduleSerializer
 
 
+class JSONGenerator:
+    @classmethod
+    def get_alarm(cls, minute="*", hour="*", day_of_week="*", command="*"):
+        template = '{{"id":null,"minute":"{}","hour":"{}",' + \
+                      '"day_of_week":"{}","command":"{}"}}'
+        formatted = template.format(minute, hour, day_of_week, command)
+        return formatted.encode()
+
+    @classmethod
+    def json_to_data(cls, json):
+        stream = BytesIO(json)
+        data = JSONParser().parse(stream)
+        return data
+
+
 class AlarmTestCase(TestCase):
     def test_alarm_serializer(self):
         """Test Django REST serializer. Model -> JSON"""
         model = AlarmSchedule(minute="*", hour="*", day_of_week="*")
         serializer = AlarmScheduleSerializer(model)
         json = JSONRenderer().render(serializer.data)
-        expected_json = b'{"minute":"*","hour":"*","day_of_week":"*",\
-                          "command":"ls"}'
+        expected_json = JSONGenerator.get_alarm(command="ls")
         self.assertEqual(json, expected_json)
-    
+
     def test_alarm_deserializer(self):
         """Test Django REST serializer. JSON -> Model"""
         # positive case
-        json = b'{"minute":"1","hour":"2","day_of_week":"3","command":"ls"}'
-        stream = BytesIO(json)
-        data = JSONParser().parse(stream)
+        json = JSONGenerator.get_alarm("1", "2", "3", "ls")
+        data = JSONGenerator.json_to_data(json)
 
         serializer = AlarmScheduleSerializer(data=data)
-        self.assertTrue(serializer.is_valid(), 
+        self.assertTrue(serializer.is_valid(),
                         "Serializer validation has failed")
 
         model = AlarmSchedule(**serializer.data)
@@ -34,12 +47,11 @@ class AlarmTestCase(TestCase):
         self.assertEqual(model.day_of_week, "3")
 
         # negative case
-        json = b'{"minute":"-1","hour":"25","day_of_week":"8","command":"ls"}'
-        stream = BytesIO(json)
-        data = JSONParser().parse(stream)
-        
+        json = JSONGenerator.get_alarm("-1", "25", "8", "ls")
+        data = JSONGenerator.json_to_data(json)
+
         serializer = AlarmScheduleSerializer(data=data)
-        self.assertFalse(serializer.is_valid(), 
+        self.assertFalse(serializer.is_valid(),
                          "Serializer validation has failed")
 
 
@@ -49,10 +61,10 @@ class AlarmSericeTestCase(TestCase):
 
     def test_read(self):
         pass
-    
+
     def test_read_all(self):
         pass
-    
+
     def test_update(self):
         pass
 
