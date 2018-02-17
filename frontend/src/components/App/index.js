a05e4bf6f5e9fb24d6f2bcd9a33ccc3624b6e96b
Andrew Kim <taeyeonkim90@gmail.com>
2018-02-16 16:28:28 -0800

    confirmed async redux action working, and connected the App component to the redux

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AlarmContainer from '../AlarmContainer'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <AlarmContainer/>
+      </MuiThemeProvider>
     );
   }
 }
