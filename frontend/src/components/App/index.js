55bf47492ec185452ba575e82c5533ab09f719fa
Andrew Kim <taeyeonkim90@gmail.com>
2018-02-16 22:54:36 -0800

    finished implementing the frontend application

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { ToastContainer, toast } from 'react-toastify';
+
 import AlarmContainer from '../AlarmContainer'
 
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <AlarmContainer/>
-      </MuiThemeProvider>
+      <div>
+        <MuiThemeProvider>
+          <AlarmContainer/>
+        </MuiThemeProvider>
+        <ToastContainer autoClose={2500} position={toast.POSITION.BOTTOM_CENTER}/>
+      </div>
     );
   }
 }
