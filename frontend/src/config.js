e3c54d1bc9d29ed14c9dd329b53d96c35494288c
Andrew Kim <taeyeonkim90@gmail.com>
2018-02-17 23:04:38 -0800

    updated where frontend sends requests to in production build

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,7 @@
-export const BASE_URL = "http://localhost:8000"
+var BASE_URL = "http://localhost:8000"
+
+if (process.env.NODE_ENV === 'production') {
+  BASE_URL = "http://192.168.0.199:8080/" 
+}
+
+export default BASE_URL
